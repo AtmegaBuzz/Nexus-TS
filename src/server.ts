40412659f07a943c34059872d08c82dcf1219c59
Prathamesh Mutkure <pmutkure009@gmail.com>
2024-05-21 15:39:53 +0530

    Minor changes

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, {Express, Request, Response} from "express";
+import express, { Express, Request, Response } from "express";
 import * as bodyParser from "body-parser";
 import cors = require("cors");
 
@@ -8,19 +8,20 @@ import deviceROuter from "./routes/device.route";
 const app: Express = express();
 const PORT = 3001;
 
-app.use(bodyParser.json({
+app.use(
+  bodyParser.json({
     verify: (req, res, buf) => {
-        console.log(buf.toString());
-    }
-}));app.use(express.json());
+      console.log(buf.toString());
+    },
+  })
+);
+app.use(express.json());
 app.use(cors());
 
 // routes
 app.use("/api/notarize", notarizeRouter);
 app.use("/api/device", deviceROuter);
 
-
-
-app.listen(PORT,()=>{
-    console.log(`Server running on port 3001`);
-})
+app.listen(PORT, () => {
+  console.log(`Server running on port 3001`);
+});
